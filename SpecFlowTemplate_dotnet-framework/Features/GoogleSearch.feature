e3793372bd6b1387cb9d7f33deedf008f97a8f50
Will Eng <wieng@netcompany.com>
2019-06-04 15:54:29 +0100

    Initial Commit. Basic Specflow project using .NET Framework

---
+++ SpecFlowTemplate_dotnet-framework/Features/GoogleSearch.feature
@@ -0,0 +1,16 @@
+﻿Feature: Google Search
+
+As an internet user
+I want to be able to search about topics I am interested in 
+So I can become better informed about these topics 
+
+Scenario Outline: Get the results for any given topic
+Given I am on the Google home page
+When I search for <topic>
+Then Each of the results should contain the <topic>
+
+Examples: 
+| topic   |
+| testing |
+| weather |
+ 
---
+++ SpecFlowTemplate_dotnet-framework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpecFlowTemplate_dotnet-framework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpecFlowTemplate_dotnet-framework")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3e070b39-68eb-437c-a348-96273d318877")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
