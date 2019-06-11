f9eff5f75f183047546c1ac044c66ee3a40652b3
Will Eng <wieng@netcompany.com>
2019-06-11 15:21:16 +0100

    Add WebElementExtension class with webdriver wait overide method

---
+++ SpecFlowTemplate_dotnet-framework/Framework/WebElementExtentions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SpecFlowTemplate_dotnet_framework.Framework
+{
+	internal static class WebElementExtensions
+	{
+
+		public static IWebElement FindElement(this IWebDriver driver, By by, int timeoitInSeconds)
+		{
+			if (timeoitInSeconds > 0)
+			{
+				var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoitInSeconds));
+				return wait.Until(drv => drv.FindElement(by));
+			}
+
+			return driver.FindElement(by);
+		}
+
+	}
+}
